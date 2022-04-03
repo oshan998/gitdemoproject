db70d82f8612b706bba116e61eba6e2004ea89cb
Oshan Lakshitha Nanayakkara <oshxn998@gmail.com>
2022-04-03 16:29:50 +0530

    change

---
+++ src/hllo.java
@@ -0,0 +1,29 @@
+public class hllo {
+
+    private int name;
+    private int salary;
+    private int vehiclenumber;
+    public Teacher(int name, int salary,int vehiclenumber) {
+        super();
+        this.name = name;
+        this.salary = salary;
+        this.vehiclenumber=vehiclenumber;
+    }
+    public int getName() {
+        return name;
+    }
+    public void setName(int name) {
+        this.name = name;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public calculate(int sum){
+        int a,b;
+        int sum= a+b;
+    }
+}
